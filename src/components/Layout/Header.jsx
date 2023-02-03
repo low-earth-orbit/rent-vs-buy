55bbc733ae9ef8b4066dbafdafc3d5535de6894d
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2023-02-03 00:50:18 -0400

    Setup React app

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Is it better to rent or buy?</h1>
+      <p>
+        A simple and sensible calculator for comparing renting vs owning a home.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main>
+      <p>
+        Start from <button>Monthly Rent</button> or{" "}
+        <button>Purchase Price</button>
+      </p>
+    </main>
+  );
+};
+
+export default Main;
